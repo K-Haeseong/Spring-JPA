9cd08f94f080896a77b1f98a077ffd36c044534d
Aiden <kim_aiden@naver.com>
2024-04-10 23:52:54 +0900

    test : JPA 기본 동작 원리 테스트

--- JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -7,10 +7,27 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloJPA");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
+
     }
 }
