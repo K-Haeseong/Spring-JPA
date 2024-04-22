6cb3675a3312881bbd23668a938aef8406d69187
Aiden <kim_aiden@naver.com>
2024-04-22 23:58:54 +0900

    test : 객체지향 쿼리 언어 - 기본 문법 테스트

---
+++ JPA-basic/jpql/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,57 @@
+package hellojpa;
+
+import hellojpa.jpql.Member;
+import hellojpa.jpql.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.awt.print.Book;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m inner join m.team.t";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            System.out.println("result = " + result);
+
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
