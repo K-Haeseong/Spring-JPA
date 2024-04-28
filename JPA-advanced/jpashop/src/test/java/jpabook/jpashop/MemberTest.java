c6a310e208fa318ce3d3a46d294fa3f9c2c4a213
Aiden <kim_aiden@naver.com>
2024-04-28 23:37:07 +0900

    feat : 회원 도메인 개발 - MemberRepository 클래스 작성
    - MemberService 클래스 작성
    - MemberServiceTest 작성 - 회원가입 및 중복회원조회 테스트

--- JPA-advanced/jpashop/src/test/java/jpabook/jpashop/MemberTest.java
+++ JPA-advanced/jpashop/src/test/java/jpabook/jpashop/MemberTest.java
@@ -1,39 +1,40 @@
-package jpabook.jpashop;
-
-import jpabook.jpashop.domain.Member;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-class MemberTest {
-
-    @Autowired
-    MemberRepository memberRepository;
-    
-    @Test
-    @Transactional
-    @Rollback(false)
-    @DisplayName("Member Test")
-    void memberTest() throws Exception {
-        //given
-        Member member = new Member();
-        member.setName("memberA");
-
-        //when
-        Long id = memberRepository.save(member);
-        Member findMember = memberRepository.find(id);
-
-        //then
-        Assertions.assertThat(member.getId()).isEqualTo(findMember.getId());
-
-        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
-
-        Assertions.assertThat(member).isEqualTo(findMember);
-
-    }
-}
+//package jpabook.jpashop;
+//
+//import jpabook.jpashop.domain.Member;
+//import jpabook.jpashop.repository.MemberRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//class MemberTest {
+//
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    @DisplayName("Member Test")
+//    void memberTest() throws Exception {
+//        //given
+//        Member member = new Member();
+//        member.setName("memberA");
+//
+//        //when
+//        Long id = memberRepository.save(member);
+//        Member findMember = memberRepository.find(id);
+//
+//        //then
+//        Assertions.assertThat(member.getId()).isEqualTo(findMember.getId());
+//
+//        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+//
+//        Assertions.assertThat(member).isEqualTo(findMember);
+//
+//    }
+//}
---
+++ JPA-advanced/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("회원가입")
+    void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("회원A");
+
+        //when
+        Long id = memberService.join(member);
+
+        //then
+        assertThat(member.getId()).isEqualTo(id);
+        assertEquals(member, memberRepository.findOne(id));
+    }
+    
+    @Test
+    @DisplayName("중복회원예외")
+    void 중복회원예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("회원B");
+
+
+        Member member2 = new Member();
+        member2.setName("회원B");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThatThrownBy(() -> memberService.join(member2)).isInstanceOf(IllegalStateException.class);
+    }
+}
