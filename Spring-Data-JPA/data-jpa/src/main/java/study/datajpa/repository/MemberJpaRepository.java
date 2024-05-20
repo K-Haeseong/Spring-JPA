8e00458930f02a52ddfd976124d80583712c14b3
Aiden <kim_aiden@naver.com>
2024-05-20 20:32:08 +0900

    feat : Spring Data JPA 공통 인터페이스 기능 - 순수 JPA 기반 Repository와Spring Data JPA 기반 Repository 비교 및 테스트

--- Spring-Data-JPA/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ Spring-Data-JPA/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,19 +5,50 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
     @PersistenceContext
     private EntityManager em;
 
+    /* 저장 */
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
+    /* 변경 */
+    // 변경감지를 사용함으로 메서드 필요 X
+
+    /* 삭제 */
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    /* 전체 조회 */
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    /* 단건 조회 */
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    /* 단건 조회(OPTIONAL) */
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    /* 카운트 */
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
 }
