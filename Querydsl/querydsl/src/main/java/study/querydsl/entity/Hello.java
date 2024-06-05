b12f80bb6e572828102572500d2a228741053597
Aiden <kim_aiden@naver.com>
2024-06-05 22:36:54 +0900

    feat : JPA 동작확인을 위한 예제 도메인 추가

--- Querydsl/querydsl/src/main/java/study/querydsl/entity/Hello.java
+++ Querydsl/querydsl/src/main/java/study/querydsl/entity/Hello.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Entity
 @Getter @Setter
--- Querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ Querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -6,12 +6,14 @@ import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
+@Rollback(value = false)
 class QuerydslApplicationTests {
 
 	@PersistenceContext
